0907121bfff98be4cd735c128f8b9faf1a7cb64a
Max Pozdeev <maxpozdeev@gmail.com>
2019-03-03 23:20:02 +0300

    show quicklook thumbnail

--- project/heic-quicklook/GeneratePreviewForURL.m
+++ project/heic-quicklook/GeneratePreviewForURL.m
@@ -18,36 +18,36 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     @autoreleasepool {
-        
+
         oHEIF *heicFile = [[oHEIF alloc] initWithFileAtPath:((__bridge NSURL*)url).path];
         CGSize size = [heicFile sizeOfPrimaryImage];
         
         if (CGSizeEqualToSize(size, CGSizeZero)) {
             return noErr;
         }
-        
-        CGContextRef context = QLPreviewRequestCreateContext(preview, size, true, NULL);
-		//CGColorSpaceRef cs = CGBitmapContextGetColorSpace(context); //no need to release //kCGColorSpaceDeviceRGB
-		CGColorSpaceRef cs = CGDisplayCopyColorSpace(CGMainDisplayID()); //RGB with monitor color profile
+		
+		NSDictionary *properties = @{ };
+        CGContextRef context = QLPreviewRequestCreateContext(preview, size, true, (__bridge CFDictionaryRef)properties);
+		CGColorSpaceRef cs = CGBitmapContextGetColorSpace(context); //no need to release //kCGColorSpaceDeviceRGB
+		//CGColorSpaceRef cs = CGDisplayCopyColorSpace(CGMainDisplayID()); //RGB with monitor color profile
 		//NSLog(@"colorspace: %@", cs);
         
-        //if ([heicFile decodeFirstImageWithColorSpace:cs])
 		BOOL decoded = [heicFile decodePrimaryImageWithColorSpace2:cs];
-		CGColorSpaceRelease(cs);
+		//CGColorSpaceRelease(cs);
 		if (decoded)
         {
             if (QLPreviewRequestIsCancelled(preview)) {
                 CGContextRelease(context);
                 return noErr;
             }
-            
+
             CGRect rect = CGRectMake(0, 0, size.width, size.height);
             CGContextDrawImage(context, rect, heicFile.cgImage);
             QLPreviewRequestFlushContext(preview, context);
         }
         CGContextRelease(context);
     }
-    
+
     return noErr;
 }
 
--- project/heic-quicklook/GenerateThumbnailForURL.m
+++ project/heic-quicklook/GenerateThumbnailForURL.m
@@ -2,6 +2,8 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
+#import "oHEIF.h"
+
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
 
@@ -13,8 +15,66 @@ void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbn
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
-    // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
-    return noErr;
+	@autoreleasepool {
+		
+		//NSLog(@"want thumb for %@, options %@, maxSize %@", url, options, NSStringFromSize(maxSize));
+		
+		NSInteger needScale = [[((__bridge NSDictionary*)options) objectForKey:@"QLThumbnailOptionScaleFactor"] integerValue];
+		CGSize needSize = maxSize;
+		if (needScale > 1) needSize = CGSizeMake(maxSize.width * needScale, maxSize.height * needScale);
+		
+		oHEIF *heicFile = [[oHEIF alloc] initWithFileAtPath:((__bridge NSURL*)url).path];
+		CGSize size = [heicFile sizeOfPrimaryImage];
+		
+		if (CGSizeEqualToSize(size, CGSizeZero)) {
+			return noErr;
+		}
+		
+		
+		CGColorSpaceRef cs = CGColorSpaceCreateDeviceRGB();
+		BOOL decoded = [heicFile decodePrimaryImageWithColorSpace2:cs];
+		if (decoded)
+		{
+			if (QLThumbnailRequestIsCancelled(thumbnail)) {
+				CGColorSpaceRelease(cs);
+				return noErr;
+			}
+
+			CGImageRef imageToShow = heicFile.cgImage;
+			
+			//make smaller for cache?
+			if (size.width > needSize.width || size.height > needSize.height)
+			{
+				CGFloat scale = MAX(size.width/needSize.width, size.height/needSize.height);
+				CGRect targetRect = CGRectIntegral(CGRectMake(0, 0, size.width/scale, size.height/scale));
+				
+				CGContextRef newImageContext = CGBitmapContextCreate (NULL, targetRect.size.width, targetRect.size.height, 8, 0, cs, kCGImageAlphaNoneSkipLast);
+				CGContextSetInterpolationQuality(newImageContext, kCGInterpolationNone);
+				CGContextDrawImage(newImageContext, targetRect, heicFile.cgImage);
+				imageToShow = CGBitmapContextCreateImage(newImageContext);
+				CFRelease(newImageContext);
+			}
+			
+			NSDictionary *properties = @{ @"IconFlavor": @(5) }; //icon mode for images
+/*
+			CGRect showRect = CGRectMake(0, 0, CGImageGetWidth(imageToShow), CGImageGetHeight(imageToShow));
+			CGContextRef context = QLThumbnailRequestCreateContext(
+																   thumbnail,
+																   showRect.size,
+																   true,
+																   (__bridge CFDictionaryRef)properties
+																   );
+			CGContextDrawImage(context, showRect, imageToShow);
+			QLThumbnailRequestFlushContext(thumbnail, context);
+*/
+	
+			QLThumbnailRequestSetImage(thumbnail, imageToShow, (__bridge CFDictionaryRef) properties);
+		}
+		CGColorSpaceRelease(cs);
+
+	}
+	
+	return noErr;
 }
 
 
@@ -22,3 +82,5 @@ void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbn
 {
     // Implement only if supported
 }
+
+

74229a5145c5ee94e9ff64ff507e0f06a468e825
maxpozdeev <maxpozdeev@gmail.com>
2025-05-14 20:37:19 +0300

    build libheif with cmake; use static only libs

--- libs/2_build_libs.sh
+++ libs/2_build_libs.sh
@@ -2,8 +2,8 @@
 
 rm -rf local
 rm -rf include
-rm -f libheif.dylib
-rm -rf libheif.dylib.dSYM
+#rm -f libheif.dylib
+#rm -rf libheif.dylib.dSYM
 rm -rf static
 mkdir local
 
@@ -16,10 +16,13 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 #export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig
 CORESCOUNT=4
 
+CONFIGURATION="RelWithDebInfo"
 if [ "$1" == "debug" ]; then
  FLAGS="${FLAGS} -g "
+ CONFIGURATION="Debug"
 elif  [ "$1" == "release" ]; then
  FLAGS="${FLAGS} -O3"
+ CONFIGURATION="Release"
 fi
 
 # libde265
@@ -46,15 +49,21 @@ cd ../..
 # libheif
 cd libheif
 
-make clean > /dev/null
-./configure --prefix="$PREFIX" --disable-go --disable-examples --disable-tests CXXFLAGS="${FLAGS}" || exit 1
-make -j$CORESCOUNT || exit 1
+rm -rf build
+mkdir build
+cd build
+cmake ../ -LA -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+  -DCMAKE_CXX_FLAGS="${FLAGS}" \
+  -DCMAKE_BUILD_TYPE=$CONFIGURATION \
+  -DWITH_EXAMPLES=OFF -DWITH_AOM=OFF -DWITH_DAV1D=ON -DWITH_RAV1E=OFF -DWITH_X265=OFF
+make -j$CORESCOUNT
 make install
-cd ..
+cd ../..
 
 
-cp -L local/lib/libheif.dylib ./
-install_name_tool -id @rpath/libheif.dylib libheif.dylib
+#cp -L local/lib/libheif.dylib ./
+#install_name_tool -id @rpath/libheif.dylib libheif.dylib
 
 mkdir include
 cp -r local/include/libheif ./include/
@@ -67,7 +76,7 @@ cp local/lib/libheif.a static/
 
 #rm -rf local
 
-if [[ "$1" == "release" ]]; then
-  dsymutil libheif.dylib -o libheif.dylib.dSYM
-  strip -S libheif.dylib
-fi
+#if [[ "$1" == "release" ]]; then
+#  dsymutil libheif.dylib -o libheif.dylib.dSYM
+#  strip -S libheif.dylib
+#fi

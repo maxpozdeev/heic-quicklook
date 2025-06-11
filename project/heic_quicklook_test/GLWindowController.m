7b1cf51df6b18edfcb9b5c8ecb08210a82d00d5c
maxpozdeev <maxpozdeev@gmail.com>
2025-06-11 19:02:46 +0300

    allow avif in CG and GL windows

--- project/heic_quicklook_test/GLWindowController.m
+++ project/heic_quicklook_test/GLWindowController.m
@@ -49,12 +49,12 @@
     [[NSDocumentController sharedDocumentController] noteNewRecentDocumentURL:[NSURL fileURLWithPath:filename]];
     [self.window setTitleWithRepresentedFilename:filename];
     
-    NSString *fileExt = filename.pathExtension.lowercaseString;
-    if (![fileExt isEqualToString:@"heic"] )
-    {
-        self.label.stringValue = [NSString stringWithFormat:@"%@ | Not a HEIC file", filename.lastPathComponent];
-        return;
-    }
+//    NSString *fileExt = filename.pathExtension.lowercaseString;
+//    if (![fileExt isEqualToString:@"heic"] )
+//    {
+//        self.label.stringValue = [NSString stringWithFormat:@"%@ | Not a HEIC file", filename.lastPathComponent];
+//        return;
+//    }
     
     dispatch_async(dispatch_get_main_queue(), ^
        {
